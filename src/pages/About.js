2b79d4518a57d1edd8646770aebe91a0aae14f29
Kirill Golubjov <kirillcnz@gmail.com>
2023-02-16 17:28:40 +0200

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const About = () => {
+  return (
+    <section className='section'>
+      <h2>About</h2>
+      <Link className='btn' to='/'>
+        back home
+      </Link>
+    </section>
+  );
+};
+export default About;
